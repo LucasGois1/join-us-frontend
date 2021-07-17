6fa3a732145b311148abee866fbf74e75a255a83
Ryan Basque <ryanbasque@icloud.com>
2021-07-17 19:01:56 -0300

    fixes: push para a main

---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../styles/colors';
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        paddingHorizontal: 30
+    }
+});
+
+export default styles;
---
+++ src/utils/screen.ts
@@ -0,0 +1,6 @@
+import { Dimensions } from 'react-native';
+
+export const ScreenHeight = Dimensions.get('screen').height;
+export const ScreenWidth = Dimensions.get('screen').width;
+export const ScreenFontScale = Dimensions.get('screen').fontScale;
+export const ScreenScale = Dimensions.get('screen').scale;
