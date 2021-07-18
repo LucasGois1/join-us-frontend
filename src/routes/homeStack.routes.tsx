c83ad7ea33b3bc90413e54487be3d954efd3b2c5
Lucas Gois <lucas.gois@linnks.com.br>
2021-07-18 10:39:49 -0300

    feat: new route Home

--- src/routes/homeStack.routes.tsx
+++ src/routes/homeStack.routes.tsx
@@ -1,36 +1,27 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import OnBording from '../pages/OnBoarding';
-import CreateAccount from '../pages/CreateAccount';
-import Login from '../pages/Login';
+import OnBording from "../pages/OnBoarding";
+import CreateAccount from "../pages/CreateAccount";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 const StackRoutes = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-    <StackRoutes.Navigator
-        headerMode="none"
-        screenOptions={{
-            cardStyle: {
-                backgroundColor: '#FFFF'
-            }
-        }}
-    >
-
-        <StackRoutes.Screen 
-            name="OnBoarding"
-            component={OnBording}
-        />
-        <StackRoutes.Screen 
-            name="CreateAccount"
-            component={CreateAccount}
-        />
-        <StackRoutes.Screen 
-            name="Login"
-            component={Login}
-        />
-
-    </StackRoutes.Navigator>
+  <StackRoutes.Navigator
+    headerMode="none"
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: "#FFFF",
+      },
+    }}
+  >
+    <StackRoutes.Screen name="OnBoarding" component={OnBording} />
+    <StackRoutes.Screen name="CreateAccount" component={CreateAccount} />
+    <StackRoutes.Screen name="Login" component={Login} />
+    <StackRoutes.Screen name="Home" component={Home} />
+  </StackRoutes.Navigator>
 );
 
 export default AppRoutes;
