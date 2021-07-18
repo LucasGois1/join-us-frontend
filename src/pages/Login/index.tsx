6dffc88bb5160322deb298c5ed32ca66da64539e
Lucas Gois <lucas.gois@linnks.com.br>
2021-07-18 10:41:12 -0300

    feat: new Page Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,126 @@
-import React from 'react';
+import { useNavigation } from "@react-navigation/native";
+import React, { useContext, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Toast from "react-native-toast-message";
+import GreenButton from "../../components/GreenButton";
+import LogoImage from "../../components/LogoImage";
+import { HttpContext } from "../../context/httpContext";
+import useNotification from "../../hooks/useNotification";
+import styles from "./styles";
 
 const Login: React.FC = () => {
+  const [email, onEmailChange] = useState("");
+  const [password, onPasswordChange] = useState("");
+  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [invalidPassword, setInvalidPassword] = useState(false);
 
-    return (
-        <>
+  const navigation = useNavigation();
 
-        </>
-    )
+  const request = useContext(HttpContext);
+
+  const handleNextPage = (screenTitle: string) => {
+    navigation.navigate(screenTitle);
+  };
+
+  const handleSubmitUser = async () => {
+    if (!email) {
+      setInvalidEmail(true);
+      useNotification("error", "Ops!", "Você precisa inserir um e-mail 😅");
+      return;
+    } else {
+      setInvalidEmail(false);
+    }
+
+    if (!password) {
+      setInvalidPassword(true);
+      useNotification("error", "Ops!", "Você precisa inserir uma senha 😅");
+      return;
+    } else {
+      setInvalidPassword(false);
+    }
+
+    const response = await request.post("/signup", {
+      email,
+      password,
+    });
+  };
+
+  return (
+    <SafeAreaView>
+      <KeyboardAvoidingView
+        style={{ marginTop: 30 }}
+        behavior="position"
+        enabled
+      >
+        <View style={styles.container}>
+          <View style={styles.imageContainer}>
+            <LogoImage />
+          </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Join Us!</Text>
+          </View>
+          <View style={styles.formContainer}>
+            <View>
+              <TextInput
+                style={{
+                  ...styles.formInput,
+                  borderBottomColor: invalidEmail
+                    ? "#E83F5B"
+                    : styles.formInput["borderBottomColor"],
+                }}
+                placeholderTextColor="#5C6660"
+                keyboardType="email-address"
+                placeholder="Digite seu email"
+                onChangeText={onEmailChange}
+                value={email}
+              />
+            </View>
+            <View>
+              <TextInput
+                style={{
+                  ...styles.formInput,
+                  borderBottomColor: invalidPassword
+                    ? "#E83F5B"
+                    : styles.formInput["borderBottomColor"],
+                }}
+                placeholderTextColor="#5C6660"
+                placeholder="Digite sua senha"
+                onChangeText={onPasswordChange}
+                value={password}
+                secureTextEntry={true}
+              />
+            </View>
+            <View style={styles.haveAccountContainer}>
+              <TouchableOpacity
+                style={styles.haveAccountButton}
+                onPress={() => handleNextPage("CreateAccount")}
+              >
+                <Text style={styles.haveAccountText}>
+                  Ainda não tenho uma conta
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.buttonContainer}>
+              <GreenButton
+                style={styles.button}
+                active={true}
+                title="Criar conta"
+                onPress={handleSubmitUser}
+              />
+            </View>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 };
 
 export default Login;
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -6,7 +6,62 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-around',
         paddingHorizontal: 30
-    }
+    },
+    imageContainer: {
+        marginTop: 20,
+        marginBottom: 15
+    },
+    titleContainer: {
+        marginBottom: 25,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleText: {
+        fontSize: 32,
+        lineHeight: 38,
+        color: colors.green_dark,
+        fontWeight: '600'
+    },
+    formContainer: {
+        width: '100%'
+    },
+    formInput: {
+        width: '100%',
+        height: 35,
+        textAlign: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: colors.dark_white,
+        marginBottom: 20
+    },
+    formInputPlaceHolder: {
+        color: colors.gray
+    },
+    haveAccountContainer: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    haveAccountButton: {
+        backgroundColor: colors.white,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    haveAccountText: {
+        textAlign: 'center'
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.green,
+        width: '80%',
+        height: 60,
+        borderRadius: 15,
+        marginTop: 20
+    },
 });
 
 export default styles;
