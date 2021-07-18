11603c3f5f13581d4fb9e6765ee1de3273f60bef
Lucas Gois <lucas.gois@linnks.com.br>
2021-07-18 10:41:57 -0300

    feat: start Home Screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
 
 const Home: React.FC = () => {
-
-    return (
-        <>
-
-        </>
-    )
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
